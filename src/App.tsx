630ad7d6e7c7e12988ebaf838991d24bc920366b
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-18 13:49:04 -0500

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import Sidebar from "./components/Sidebar";
+
+const App = () =>{
+  return (
+    <div className="">
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+interface Product {
+  category: string;
+}
+
+interface FetchResponse {
+  products: Product[];
+}
+
+const Sidebar = () => {
+  const [categories, setCategories] = useState<string[]>([]);
+  const [keywords] = useState<string[]>([
+    "apple",
+    "watch",
+    "Fashion",
+    "trend",
+    "shoes",
+    "shirt",
+  ]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data: FetchResponse = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCategories();
+  }, []);
+  return <div></div>;
+};
+
+export default Sidebar;
